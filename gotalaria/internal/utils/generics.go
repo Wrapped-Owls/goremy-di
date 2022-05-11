2e6eb3f83268186dfd12c26d4e5120e9b94d7cd0
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-05-11 17:51:41 -0300

    :wrench: fix: qualify interfaces by its complete names

--- gotalaria/internal/utils/generics.go
+++ gotalaria/internal/utils/generics.go
@@ -10,22 +10,27 @@ func GetKey[T any]() types.BindKey {
 	return TypeName[T]()
 }
 
+// TypeName returns a string that defines the name of the given generic type.
+//
+// TODO: Create a typeNameInterface that generates the name based on interface methods signature,
+// TODO: so it can be used without importing interfaces (add a flag for it)
 func TypeName[T any]() string {
-	elementType := GetType[T]()
+	elementType, _ := GetType[T]()
 	if elementType == nil {
 		panic(ErrImpossibleIdentifyType)
 	}
-	return fmt.Sprintf("%s/%s", elementType.PkgPath(), elementType.Name())
+	return fmt.Sprintf("%s/%s{###}%s", elementType.PkgPath(), elementType.Name(), fmt.Sprint(elementType))
 }
 
-func GetType[T any]() reflect.Type {
+func GetType[T any]() (foundType reflect.Type, isInterface bool) {
 	var typeT T
-	foundType := reflect.TypeOf(typeT)
+	foundType = reflect.TypeOf(typeT)
 	if foundType == nil {
 		// T is an interface
+		isInterface = true
 		foundType = reflect.TypeOf(&typeT)
 	}
-	return foundType
+	return
 }
 
 func Default[T any]() T {
