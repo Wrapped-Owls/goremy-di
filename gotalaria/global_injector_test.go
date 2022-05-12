e9d7926ad502f228b21dd8e28eb87e39d0cd6093
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-05-12 01:27:02 -0300

    :keyboard: test: Test properly global injector
    Remove pre-register to do get multi thread

--- gotalaria/global_injector_test.go
+++ gotalaria/global_injector_test.go
@@ -6,18 +6,14 @@ import (
 )
 
 func TestGlobal_GetWithConcurrency(t *testing.T) {
-	const expected = "it's working, yaaay!"
 	var wg sync.WaitGroup
 
-	Register(nil, Instance(func(retriever DependencyRetriever) string {
-		return expected
-	}))
 	for i := 0; i < 255; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			if Get[string](nil) != expected {
-				t.Error("string obtained is not the same as expected")
+			if len(Get[string](nil)) != 0 {
+				t.Error("string retrieved from global injector should be empty")
 			}
 		}()
 	}
