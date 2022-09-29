57b16b121be953096398834b207707848178cb52
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-09-29 03:40:40 -0300

    :pager: feat: Update logger example to log in more than 1 output

--- examples/bindlogger/main.go
+++ examples/bindlogger/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/wrapped-owls/goremy-di/remy"
 	"github.com/wrapped-owls/talaria-di/examples/bindlogger/utils"
 )
 
 func main() {
-	lInject := utils.NewLoggerInjector(utils.DefaultLogger{Level: utils.LevelDefault | utils.LevelInfo})
+	lInject := utils.NewLoggerInjector(
+		utils.DefaultLogger{
+			ErrOutput: log.New(os.Stderr, "[Error]", log.LstdFlags|log.Lshortfile),
+			StdOutput: log.New(os.Stdout, "", log.LstdFlags),
+			Level:     utils.LevelDefault | utils.LevelInfo,
+		},
+	)
 	remy.RegisterInstance(lInject, "The Lord of the Rings", "movie")
 	remy.Register(
 		lInject, remy.Factory(
