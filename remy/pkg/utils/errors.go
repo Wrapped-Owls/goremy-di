e44deab8fd57d4e81d31dfdf22376e5c8b17623b
João Victor Oliveira Couto <commits@jictyvoo.dev>
2023-04-03 21:39:03 -0300

    :minidisc: feat: [wip] Declare new errors and methods

--- remy/pkg/utils/errors.go
+++ remy/pkg/utils/errors.go
@@ -6,8 +6,13 @@ var (
 	ErrAlreadyBound                 = errors.New("dependency already bound")
 	ErrImpossibleIdentifyType       = errors.New("impossible to identify the given type")
 	ErrElementNotRegistered         = errors.New("element with given key is not registered")
-	ErrNoElementFoundInsideOrParent = errors.New("no element found on the given injector or any of it's parents")
-	ErrCycleDependencyDetected      = errors.New("cycle dependency detected, check for it")
-	ErrOverrideInRuntime            = errors.New("a process tried to override a value during runtime")
-	ErrTypeCastInRuntime            = errors.New("unable to find/cast the element with given type")
+	ErrNoElementFoundInsideOrParent = errors.New(
+		"no element found on the given injector or any of it's parents",
+	)
+	ErrCycleDependencyDetected = errors.New("cycle dependency detected, check for it")
+	ErrOverrideInRuntime       = errors.New(
+		"a process tried to override a value during runtime",
+	)
+	ErrTypeCastInRuntime       = errors.New("unable to find/cast the element with given type")
+	ErrFoundMoreThanOneValidDI = errors.New("found more than one element that fits the given type")
 )
