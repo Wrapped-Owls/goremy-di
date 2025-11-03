3ab8b4ea625cb90bef4674ac01c40f45998a8527
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-11-02 22:34:12 -0300

    :bento: feat: Create a new wrapper for internal errors on public api

---
+++ remy/errors.go
@@ -0,0 +1,16 @@
+package remy
+
+import (
+	"github.com/wrapped-owls/goremy-di/remy/internal/errors"
+)
+
+// Re-export errors from internal/errors package for backward compatibility
+var (
+	ErrAlreadyBound                            = errors.ErrAlreadyBoundSentinel
+	ErrImpossibleIdentifyType                  = errors.ErrImpossibleIdentifyTypeSentinel
+	ErrElementNotRegistered                    = errors.ErrElementNotRegisteredSentinel
+	ErrCycleDependencyDetected                 = errors.ErrCycleDependencyDetectedSentinel
+	ErrTypeCastInRuntime                       = errors.ErrTypeCastInRuntimeSentinel
+	ErrFoundMoreThanOneValidDI                 = errors.ErrFoundMoreThanOneValidDISentinel
+	ErrGetElementTypeRequiresReflectionEnabled = errors.ErrGetElementTypeRequiresReflectionEnabled
+)
--- remy/pkg/utils/errors.go
+++ remy/pkg/utils/errors.go
@@ -3,16 +3,5 @@ package utils
 import "errors"
 
 var (
-	ErrAlreadyBound                 = errors.New("dependency already bound")
-	ErrImpossibleIdentifyType       = errors.New("impossible to identify the given type")
-	ErrElementNotRegistered         = errors.New("element with given key is not registered")
-	ErrNoElementFoundInsideOrParent = errors.New(
-		"no element found on the given injector or any of it's parents",
-	)
-	ErrCycleDependencyDetected = errors.New("cycle dependency detected, check for it")
-	ErrOverrideInRuntime       = errors.New(
-		"a process tried to override a value during runtime",
-	)
-	ErrTypeCastInRuntime       = errors.New("unable to find/cast the element with given type")
-	ErrFoundMoreThanOneValidDI = errors.New("found more than one element that fits the given type")
+	ErrAlreadyBound = errors.New("dependency already bound")
 )
