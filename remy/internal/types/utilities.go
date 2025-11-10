c454cfb91d5682e8046bea9b100bcacd900b4e51
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-11-10 03:03:06 -0300

    :bulb: feat: Add NewBindKey and enhance GetWithPairs to receive BindKey

--- remy/internal/types/utilities.go
+++ remy/internal/types/utilities.go
@@ -2,6 +2,9 @@ package types
 
 type (
 	InstancePair[T any] struct {
+		// Key is the direct generated BindKey without the automation provided by remy default functions
+		Key BindKey
+
 		// Value that will be injected directly. (required)
 		Value T
 
