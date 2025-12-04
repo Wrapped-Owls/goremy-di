388dc6216288d442f031355026c429b794fbd2b4
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-12-04 01:07:58 -0300

    :wrench: feat: Create new method on KeyElem allowing unique ID generation

--- remy/internal/types/type_key.go
+++ remy/internal/types/type_key.go
@@ -1,8 +1,13 @@
 package types
 
+import (
+	"unsafe"
+)
+
 type (
 	BindKey interface {
 		comparable()
+		ID() uint64
 	}
 
 	BindDependencies[T any] map[BindKey]T
@@ -16,4 +21,10 @@ type (
 
 func (k KeyElem[T]) comparable() {
 	// Just a stub function
+}
+
+func (k KeyElem[T]) ID() uint64 {
+	iface := any(k)
+	typePointer := uintptr(*(*unsafe.Pointer)(unsafe.Pointer(&iface)))
+	return uint64(typePointer)
 }
