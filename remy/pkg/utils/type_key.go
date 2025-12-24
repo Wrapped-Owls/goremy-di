d2a0053aaaad44084b14179ce0bf547c6b940ef6
João Victor Oliveira Couto <commits@jictyvoo.dev>
2025-12-24 02:09:23 -0300

    :hear_no_evil: feat: [wip] Remove reflection-based type key generation

--- remy/pkg/utils/type_key.go
+++ remy/pkg/utils/type_key.go
@@ -1,37 +1,8 @@
 package utils
 
 import (
-	remyErrs "github.com/wrapped-owls/goremy-di/remy/internal/errors"
 	"github.com/wrapped-owls/goremy-di/remy/internal/types"
-	"github.com/wrapped-owls/goremy-di/remy/pkg/injopts"
 )
-
-func shouldGenerify(options injopts.KeyGenOption) bool {
-	return options&injopts.KeyOptGenerifyInterface == injopts.KeyOptGenerifyInterface
-}
-
-func shouldUseReflection(options injopts.KeyGenOption) bool {
-	return options&injopts.KeyOptUseReflectionType == injopts.KeyOptUseReflectionType
-}
-
-func shouldPrefixPointer(options injopts.KeyGenOption) bool {
-	return options&injopts.KeyOptIgnorePointer != injopts.KeyOptIgnorePointer
-}
-
-func GetKey[T any](options injopts.KeyGenOption) types.BindKey {
-	generifyInterface := shouldGenerify(options)
-	var keyVal string
-	if useReflection := shouldUseReflection(options); useReflection {
-		keyVal, _ = TypeNameByReflection[T](generifyInterface, shouldPrefixPointer(options))
-	} else if generifyInterface && IsInterface[T]() { // Only retrieve key if is an interface
-		keyVal, _ = TypeNameByReflection[T](true, shouldPrefixPointer(options))
-	}
-
-	if keyVal != "" {
-		return types.StrKeyElem(keyVal)
-	}
-	return NewKeyElem[T]()
-}
 
 // IsInterface reports whether T is an interface type.
 func IsInterface[T any]() bool {
@@ -58,14 +29,4 @@ func IsInterface[T any]() bool {
 
 func NewKeyElem[T any]() types.KeyElem[T] {
 	return types.KeyElem[T]{}
-}
-
-func GetElemKey(element any, options injopts.KeyGenOption) (types.BindKey, error) {
-	if !shouldUseReflection(options) {
-		return types.StrKeyElem(""), remyErrs.ErrGetElementTypeRequiresReflectionEnabled
-	}
-
-	generifyInterface := shouldGenerify(options)
-	keyVal, err := TypeNameByReflection(generifyInterface, shouldPrefixPointer(options), element)
-	return types.StrKeyElem(keyVal), err
 }
