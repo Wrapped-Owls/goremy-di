7ee7b888f92437a9aaff79f1a56939e5b19fac80
João Victor Oliveira Couto <commits@jictyvoo.dev>
2026-08-15 14:46:32 -0300

    :open_file_folder: refactor: Move `stgbind` to a new package

--- remy/internal/injector/methods.go
+++ remy/internal/injector/methods.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 
 	remyErrs "github.com/wrapped-owls/goremy-di/remy/internal/errors"
-	"github.com/wrapped-owls/goremy-di/remy/internal/stgbind"
+	"github.com/wrapped-owls/goremy-di/remy/internal/injcontainer/stgbind"
 	"github.com/wrapped-owls/goremy-di/remy/internal/types"
 	"github.com/wrapped-owls/goremy-di/remy/pkg/injopts"
 	"github.com/wrapped-owls/goremy-di/remy/pkg/utils"
